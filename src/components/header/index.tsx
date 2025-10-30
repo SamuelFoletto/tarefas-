645eb8c1765bfb78490f462c4c28553ab7b6fbe3
Samuel <sfoletto@gmail.com>
2025-10-29 21:18:48 -0300

    Criação projeto e header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,25 @@
+import styles from "@/components/header/styles.module.css"
+import Link from "next/link"
+
+
+export function Header(){
+    return(
+        <header className={styles.header}>
+            <section className={styles.content}>
+                <nav className={styles.nav}>
+                    <Link href="/">
+                        <h1 className={styles.logo}>Tarefas <span>+</span></h1>
+                    </Link>
+
+                    <Link href="/dashboard"  className={styles.link}>
+                        Meu painel
+                    </Link>
+                </nav>
+
+                <button className={styles.loginButton}>
+                    Acessar
+                </button>
+            </section>
+        </header>
+    )
+}
